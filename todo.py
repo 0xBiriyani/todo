8828beb76a9cfdb17dc37961d99c2b1e10448f89
Naveen Pothana <Naveen.Pothana7@gmail.com>
2023-04-19 21:45:04 +0530

    formated code using black

--- todo.py
+++ todo.py
@@ -1,24 +1,29 @@
 ###################################################
 # This is todo Application which creates, updates,#
-# delete todo task.                               #  
+# delete todo task.                               #
 #                                                 #
 ###################################################
 import sys
 import os
 import time
-clear = lambda: os.system('cls')  # clear terminal
+
+clear = lambda: os.system("cls")  # clear terminal
 todos = []
+
+
 def show_main_menu():
     print("Welcome to TODO Application")
-    print(""" 
+    print(
+        """ 
     1. View Todo
     2. Create Todo
     3. Update Todo
     4. Delete Todo
     5. Exit
-    """)
+    """
+    )
     choice = input("Choose number to perform action:").strip()
-    if choice in ["1","2","3","4","5"]:
+    if choice in ["1", "2", "3", "4", "5"]:
         if choice == "5":
             print("Bye")
             sys.exit()
@@ -27,27 +32,32 @@ def show_main_menu():
     else:
         clear()
         print("Invalid Section, Please Select Proper Choice")
- 
+
+
 def show_create_todo_menu():
+    """_summary_"""
     clear()
     todo_name = input("Enter Todo Name:")
     todo_due_date = input("Enter Todo Due Date(YYYY-MM-DD):")
-    todos.append({"todo_name":todo_name,"todo_due_date":todo_due_date})
+    todos.append({"todo_name": todo_name, "todo_due_date": todo_due_date})
     print("Todo Added to your list :)")
     time.sleep(1)
     print("Returning to Main Menu...")
     time.sleep(1)
     clear()
- 
+
+
 def main_menu():
     clear()
     selected_choice = show_main_menu()
     while selected_choice is None:
         selected_choice = show_main_menu()
- 
+
     if selected_choice == "1":
-        for id,todo in enumerate(todos):
-            print(f"ID: {id}, Name: {todo.get('todo_name')}, Due_Date: {todo.get('todo_due_date')}")
+        for id, todo in enumerate(todos):
+            print(
+                f"ID: {id}, Name: {todo.get('todo_name')}, Due_Date: {todo.get('todo_due_date')}"
+            )
         input("press enter key to go to main menu: ")
         main_menu()
     elif selected_choice == "2":
