e4a1d4e9342d160ee3f69bb631ea70802d7f2094
Sampath <kantipudisampath007@gmail.com>
2023-04-20 16:48:07 +0530

    even_or_odd function

---
+++ sampath.py
@@ -0,0 +1,11 @@
+print('Hello Guys')
+def even_odd():
+    num = int(input(" "))
+    if num %2 ==0:
+        print("even")
+    else:
+        print("odd")
+        
+result = even_odd()
+print(result)
+5
