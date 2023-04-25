eec6f3b9f7a13ca39626acc8233a3a2ab315d61e
Sampath <kantipudisampath007@gmail.com>
2023-04-25 14:00:07 +0530

    ..

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("hello world")
+import pyautogui as pt 
+import time
+
+limit =  input ("enter limit: ")
+message = input ("Enter message: ")
+i=0
+time.sleep(3)
+
+while i<int(limit):
+    pt.typewrite(message)
+    pt.press("enter")
+
+    i+=1
+   
