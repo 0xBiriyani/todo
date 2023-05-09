39d6dbc704a9cfd9cd53d59d8569708b84d0aca4
Naveen Pothana <Naveen.Pothana7@gmail.com>
2023-05-09 21:49:04 +0530

    Added calculator Menu

---
+++ calculator/calculator.py
@@ -0,0 +1,16 @@
+############################################
+#      Welcome To Python Calculator App    #
+#                                          #
+############################################
+
+choice = input("""
+Welcome To Python Calculator
+
+1. Addtion
+2. Division
+3. Subtraction
+4. Multiplication
+5. Squreroot
+6. Exit
+Select your choice:"""
+)
