7a89580cd89b44c95373a4b531a4a4ae8adc0481
Sampath <kantipudisampath007@gmail.com>
2023-05-10 00:48:34 +0530

    subtraction module is updated

--- calculator/subtraction.py
+++ calculator/subtraction.py
@@ -1,8 +1,15 @@
-def subtraction(num1,num2):
-    return num1-num2
-num1 = input("Enter the first number: ")
-num2 = input("Enter the second number: ")
-num1 = float(num1)
-num2 = float(num2)
-result = subtraction(num1, num2)
-print (f"The result of {num1} and {num2} is:{result}")
+def subtraction():
+    num1 = input("Enter the first number: ")
+    num2 = input("Enter the second number: ")
+    try:
+        num1 = float(num1)
+        num2 = float(num2)
+        result = num1-num2
+        print(f"The result of {num1} - {num2} is {result}")
+    except ValueError:
+        print("Invalid input")
+        subtraction()
+
+if __name__ == "__main__":
+    subtraction()
+
