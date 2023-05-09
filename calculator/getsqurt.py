0dad6743ea52b7acf47c39bd05d504a56bed43e3
pathuri priyanka <pathuripriyanka@gmail.com>
2023-05-10 00:25:35 +0530

    squareroot function modified

--- calculator/getsqurt.py
+++ calculator/getsqurt.py
@@ -2,14 +2,61 @@
 #  Square root Module  #
 ##############################
 
+import math
+    
+
+
 def getsqurt():
     num1=input("Enter a number:")
     try:
         num1=float(num1)
-        print (num1*num1)
+        print (math.sqrt(num1))
     except:
         print("invalid input")
         getsqurt()
-
-if __name__ == "__main__":
-    getsqurt()
+        
+getsqurt()    
+def getaddition():
+    num1=input("Enter a number:")
+    num2=input("Enter a number:")
+    try:
+        num1=float(num1)
+        num2=float(num1)
+        print (num1+num1)
+    except:
+        print("invalid input")
+        getaddition()
+        
+def getsubtraction():
+    num1=input("Enter a number:")
+    num2=input("Enter a number:")
+    try:
+        num1=float(num1)
+        num2=float(num1)
+        print (num1-num1)
+    except:
+        print("invalid input")
+        getsubtraction()
+        
+def getmultiplication():
+    num1=input("Enter a number:")
+    num2=input("Enter a number:")
+    try:
+        num1=float(num1)
+        num2=float(num1)
+        print (num1*num1)
+    except:
+        print("invalid input")
+        getmultiplication()
+     
+def getdivision():
+    num1=input("Enter a number:")
+    num2=input("Enter a number:")
+    try:
+        num1=float(num1)
+        num2=float(num1)
+        print (num1/num1)
+    except:
+        print("invalid input")
+        getdivision()                
+    
