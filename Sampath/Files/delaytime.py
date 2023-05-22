b447745a97a7c051ba728999dab4fdda0f8bfbbb
Sampath <kantipudisampath007@gmail.com>
2023-05-22 15:22:41 +0530

    updated

--- Sampath/Files/delaytime.py
+++ Sampath/Files/delaytime.py
@@ -14,4 +14,31 @@ while True:
 # Print current time with the specified delay
     for i in range(delay):
         print(time.ctime())
+        time.sleep(1)
+
+'''
+import time
+import os
+
+# Specify the file path to read the delay value
+file_path = 'E:\github.com\todo\Sampath\Files\secondsfile.py'
+
+while True:
+    # Read the delay value from the file
+    with open(file_path, 'r') as file:
+        delay = float(file.read())
+
+    # Print the current time
+    print(time.ctime())
+
+    # Wait for the specified delay
+    time.sleep(delay)'''
+file = open("passsecondsfile.txt", "r")
+seconds=int(file.read())
+while True:
+    time.sleep(seconds)
+    for i in range (seconds):
         print(i)
+    
+    print(time.ctime())    
+    
