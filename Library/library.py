a677958a4d79de562a5a65676ee7871e0c13eace
Naveen Pothana <Naveen.Pothana7@gmail.com>
2023-05-25 22:38:34 +0530

    Added Llbrary app scaffolding

---
+++ Library/library.py
@@ -0,0 +1,47 @@
+import json
+
+class Library:
+    def __init__(self):
+        with open('books.json') as f:
+            self.books = json.load(f)
+
+    def store_book(self, book:dict):
+        # get All book ids
+        bookids = [book['id'] for book in self.books]
+
+        # check if book id is in bookids
+        if book['id'] in bookids:
+            for _book in self.books:
+                # check if book id is equal to _book id
+                if _book['id'] == book['id']:
+                    # increment quantity
+                    _book['quantity'] += 1
+        else:
+            # add new books to book
+            self.books.append(book)
+        
+        with open('books.json', 'w') as f:
+            json.dump(self.books, f, indent=4)
+
+    def retrive_book(self, id:int):
+        return self.books
+    
+    # def __get_book_by_id(self,id:int):
+    #     for book in self.books:
+    #         if book['id'] == id:
+    #             return book
+
+
+
+mylib = Library()
+
+print(mylib.books)
+
+
+mylib.store_book({
+    "id": 5,
+    "title": "The Alchemist",
+    "quantity": 1
+})
+
+print(mylib.books)
